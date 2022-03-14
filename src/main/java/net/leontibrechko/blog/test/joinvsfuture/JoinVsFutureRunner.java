651f38634e2c354d2344df449f799c96a0e599b7
leontibrecko <6NcYZPy3M9Xh7!QH>
2022-03-13 18:25:38 -0700

    Move to .net domain

--- src/main/java/net/leontibrechko/blog/test/joinvsfuture/JoinVsFutureRunner.java
+++ src/main/java/net/leontibrechko/blog/test/joinvsfuture/JoinVsFutureRunner.java
@@ -1,4 +1,4 @@
-package com.leontibrechko.blog.test.joinvsfuture;
+package net.leontibrechko.blog.test.joinvsfuture;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
